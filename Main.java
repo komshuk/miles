9e6c3f85859c7291d814258cdb62b6e8a0f5da84
komshuk <komshuk@yandex.ru>
2021-09-09 12:06:04 +0300

    Commit1

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 20_000;
+        int costMile = 20;
+        int accruedMiles = ticketPrice / costMile;
+        System.out.println(accruedMiles);
+    }
+}
